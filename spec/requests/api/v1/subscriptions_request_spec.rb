53abd183851ee992957b71418ca3ff9032e48248
Austin Moore <austinchristianmoore@gmail.com>
2022-06-11 16:02:33 -0400

    Feature: MVP functionality

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe 'subscriptions' do
     expect(response).to be_successful
     expect(new_count).to eq(count)
   end
+
+  it "cancels a subscription" do
+    customer = Customer.create!(first_name: "First Name", last_name: "Last Name", email: "fake@email.com", address: "Fake Address")
+    tea = Tea.create!(title: "Title", description: "This description can be very long because it is text", temperature: 180, brew_time: 300)
+    subscription = Subscription.create!(customer_id: customer.id, tea_id: tea.id, title: "Title", price: 399, status: "subscribed", frequency: 1)
+
+    patch "/api/v1/subscriptions/#{subscription.id}"
+    updated_subscription = Subscription.find(subscription.id)
+    
+    expect(response).to be_successful
+    expect(updated_subscription.status).to eq("unsubscribed")
+  end
 end
