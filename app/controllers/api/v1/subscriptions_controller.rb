74038a458d851a0c46652b848bdf3bbe88c7f069
Austin Moore <austinchristianmoore@gmail.com>
2022-06-11 15:38:06 -0400

    Feature: MVP functionality

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(params[:tea_id])
+    subscription = Subscription.create!(customer_id: customer.id,
+                                        tea_id: tea.id,
+                                        title: params[:title],
+                                        price: params[:price],
+                                        status: params[:status],
+                                        frequency: params[:frequency]
+                                        )
+  end
+end
